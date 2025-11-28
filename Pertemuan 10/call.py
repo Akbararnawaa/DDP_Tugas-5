36e66b18a5ac3f54ef22423c1b015623deea6e70
Akbararnawaa <akbararnawa@gmail.com>
2025-11-28 15:37:50 +0700

    Add files via upload

---
+++ Pertemuan 10/call.py
@@ -0,0 +1,15 @@
+import bangundatar, bangunruang
+
+print("========luas bangun datar========")
+print(f"luas persegi : {bangundatar.luas_persegi(5)}")
+print(f"luas segitiga : {bangundatar.luas_segitiga(5,5)}")
+print(f"luas lingkaran : {bangundatar.luas_lingkaran(5)}")
+print(f"luas ketupat : {bangundatar.luas_ketupat(5,5)}")
+print(f"luas jajargenjang : {bangundatar.luas_jajargenjang(5,5)}")
+
+print("========luas bangun ruang========")
+print(f"luas kubus : {bangunruang.luas_kubus(5)}")
+print(f"luas balok : {bangunruang.luas_balok(5,5,5)}")
+print(f"luas bola : {bangunruang.luas_bola(5)}")
+print(f"luas tabung : {bangunruang.luas_tabung(5,5)}")
+print(f"luas kerucut : {bangunruang.luas_kerucut(5,5)}")
